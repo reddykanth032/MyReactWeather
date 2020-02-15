a44a89134ce1747941af5aceabea24a8d6e8244b
Kanth Reddy <kanth.reddyb032@gmail.com>
2020-02-15 11:08:36 +0530

    added errormodel component and injected  to weather component

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,33 +2,39 @@ var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 var openWeatherMap = require('openWeatherMap');
+var ErrorModel=require('ErrorModel');
 
 var Weather = React.createClass({
   getInitialState: function () {
     return {
       isLoading:false
+
     }
   },
   handleSearch: function (location) {
     var that = this;
 
-    debugger;
-this.setState({isLoading:true});
+    // debugger;
+this.setState({isLoading:true,
+errorMessage:undefined});
     openWeatherMap.getTemp(location).then(function (temp) {
+
       that.setState({
         location: location,
         temp: temp,
         isLoading:false
       });
-    }, function (errorMessage) {
+    }, function (e) {
       that.setState({
-        isLoading:false
+        isLoading:false,
+        errorMessage:e.message
       });
-        alert(errorMessage);
+
+        // alert(errorMessage);
     });
   },
   render: function () {
-    var {isLoading ,temp, location} = this.state;
+    var {isLoading ,temp, location,errorMessage} = this.state;
 function renderMessage(){
   if(isLoading){
   return  <h3 className="text-center">Fetching Weather Info...</h3>;
@@ -36,11 +42,19 @@ function renderMessage(){
     return  <WeatherMessage temp={temp} location={location}/>;
   }
 }
+function renderError(){
+    // console.log("error message : "+errorMessage);
+    if(typeof errorMessage ==='string'){
+      return <ErrorModel message={errorMessage}/>;
+
+    }
+}
     return (
       <div>
         <h1 className="text-center">Get Weather </h1>
         <WeatherForm onSearch={this.handleSearch}/>
          {renderMessage()}
+         {renderError()}
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,8 @@ module.exports = {
       WeatherMessage: 'app/components/WeatherMessage.jsx',
       About: 'app/components/About.jsx',
       Examples: 'app/components/Examples.jsx',
-      openWeatherMap: 'app/api/openWeatherMap.jsx'
+      openWeatherMap: 'app/api/openWeatherMap.jsx',
+      ErrorModel:'app/components/ErrorModel.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
